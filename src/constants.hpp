f7ff81b27a0fdca3c6f8ddcc0834de753894e64b
LeonByrne <leonbyrne52@gmail.com>
2025-10-24 14:39:22 +0100

    New constraints file containing constants, uses ifnefs to allow compile time substitution.

---
+++ src/constants.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#ifndef DEFAULT_STARTING_SIZE
+    #define DEFAULT_STARTING_SIZE 256
+#endif
+
+#ifndef DEFAULT_STARTING_LENGTH
+    #define DEFAULT_STARTING_LENGTH 5
+#endif
+
+#ifndef DEFAULT_GENERATION_LIMIT
+    #define DEFAULT_GENERATION_LIMIT 25
+#endif
+
+#ifndef MIN_ADDITION_NUM
+    #define MIN_ADDITION_NUM 1
+#endif
+
+#ifndef MIN_REMOVAL_NUM
+    #define MIN_REMOVAL_NUM 1
+#endif
+
+#ifndef MIN_MUTATION_NUM
+    #define MIN_MUTATION_NUM 1
+#endif
+
+#ifndef MAX_ADDITION_NUM
+    #define MAX_ADDITION_NUM 1
+#endif
+
+#ifndef MAX_REMOVAL_NUM
+    #define MAX_REMOVAL_NUM 1
+#endif
+
+#ifndef MAX_MUTATION_NUM
+    #define MAX_MUTATION_NUM 1
+#endif
